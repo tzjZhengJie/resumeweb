f93e01a344414f596bb380fc821e8350ab9d37d5
tngrj <tngrenjie@yahoo.com.sg>
2024-01-04 03:42:12 +0800

    Updating to use SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>By Renjie</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+    <link rel="manifest" href="favicon/site.webmanifest" />
+    <script defer src="https://use.fontawesome.com/releases/v6.1.0/js/all.js"></script>
+</head>
+<body>
+    <section class="hero has-background-success-dark is-fullheight">
+        <div class="hero-body">
+            <div class="container is-fluid"></div>
+            <div class="container is-fluid">
+                <div>
+                    <p class="title has-text-primary-light">Tng Renjie</p>
+                    <p class="subtitle has-text-white-bis ml-1">
+                        Electrical & Electronic Engineering Undergraduate
+                    </p>
+                </div>
+                <div class="has-background-success-dark has-text-primary-light mt-2">
+                    <a class="has-text-primary-light" href="https://www.linkedin.com/in/tng-renjie/">
+                        <span class="icon-text">
+                            <span class="icon">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </span>
+                            <span>LinkedIn</span>
+                        </span>
+                    </a>
+                    <a class="has-text-primary-light" href="https://github.com/tngrj">
+                        <span class="icon-text">
+                            <span class="icon">
+                                <i class="fa-brands fa-github-alt"></i>
+                            </span>
+                            <span>Github</span>
+                        </span>
+                    </a>
+                    <a class="has-text-primary-light" href="mailto:rtng001@e.ntu.edu.sg">
+                        <span class="icon-text">
+                            <span class="icon">
+                                <i class="fa-regular fa-envelope"></i>
+                            </span>
+                            <span>Email</span>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
