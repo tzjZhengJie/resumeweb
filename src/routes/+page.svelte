a7fbb52ebe04ce1a61ca9c6a21b2809898bca27f
tngrj <tngrenjie@yahoo.com.sg>
2024-01-04 03:50:10 +0800

    Remove favicon links from page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>By Renjie</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" />
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
-    <link rel="manifest" href="favicon/site.webmanifest" />
     <script defer src="https://use.fontawesome.com/releases/v6.1.0/js/all.js"></script>
 </head>
 <body>
