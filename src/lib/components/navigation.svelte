cc98bc26ca5fbd49d056ffe1bd70a2a52de6f16d
tngrj <tngrenjie@yahoo.com.sg>
2024-01-08 05:12:25 +0800

    Update navigation and homepage to be mobile friendly

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -9,27 +9,42 @@
 <div>
 	<TabGroup
 		justify="justify-center"
-		active="variant-ghost-secondary"
-		hover="hover:variant-soft-secondary"
+		active="bg-gradient-to-br variant-gradient-warning-success text-surface-600"
+		hover="hover:bg-gradient-to-br variant-gradient-warning-success text-surface-600"
 		flex="flex-1 lg:flex-none"
 		rounded="rounded-lg"
-		border="border-0"
+		border="border-0 rounded-lg outline outline-offset-2 outline-primary-500"
 		class="text-surface-600-300-token rounded-lg fixed z-50 -translate-x-1/2 bottom-8 left-1/2 bg-surface-300-600-token w-auto"
 	>
 		<TabAnchor href="/" selected={$page.url.pathname === '/'}>
-			<svelte:fragment slot="lead"><div class={nav}><Home />Home</div></svelte:fragment>
+			<svelte:fragment slot="lead">
+				<div class={nav}>
+					<Home />
+					<span class="hidden md:inline">Home</span>
+				</div>
+			</svelte:fragment>
 		</TabAnchor>
 		<span class="mx-2"></span>
 		<TabAnchor href="/project" selected={$page.url.pathname === '/project'}>
-			<svelte:fragment slot="lead"><div class={nav}><GanttChart />Projects</div></svelte:fragment>
+			<svelte:fragment slot="lead">
+				<div class={nav}>
+					<GanttChart />
+					<span class="hidden md:inline">Projects</span>
+				</div>
+			</svelte:fragment>
 		</TabAnchor>
 		<span class="mx-2"></span>
 		<TabAnchor href="/about" selected={$page.url.pathname === '/about'}>
-			<svelte:fragment slot="lead"><div class={nav}><User />About</div></svelte:fragment>
+			<svelte:fragment slot="lead">
+				<div class={nav}>
+					<User />
+					<span class="hidden md:inline">About</span>
+				</div>
+			</svelte:fragment>
 		</TabAnchor>
 	</TabGroup>
 </div>
 
-<div class="fixed bottom-12 right-8">
+<div class="fixed top-8 right-8">
 	<LightSwitch />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,21 @@
 <script>
 	import { Linkedin, Github, Mail } from 'lucide-svelte';
+
+	const links = 'flex items-center mb-2 sm:mb-0 sm:mr-2';
 </script>
 
 <div class="text-white flex items-center justify-center h-full">
 	<div class="text-start">
-		<p class="text-4xl">Tng Renjie</p>
-		<p class="text-xl mt-2">Electrical & Electronic Engineering Undergraduate</p>
-		<div class="text-sm flex flex-row mt-2">
-			<a class="flex items-center align-middle mr-2" href="https://www.linkedin.com/in/tng-renjie/">
+		<p class="text-2xl sm:text-4xl">Tng Renjie</p>
+		<p class="text-lg sm:text-xl mt-2">Electrical & Electronic Engineering Undergraduate</p>
+		<div class="flex flex-col items-left sm:flex-row text-xs sm:text-sm mt-2">
+			<a class={links} href="https://www.linkedin.com/in/tng-renjie/">
 				<Linkedin />&nbsp;LinkedIn
 			</a>
-			<a class="flex align-middle mr-2" href="https://github.com/tngrj">
+			<a class={links} href="https://github.com/tngrj">
 				<Github />&nbsp;Github
 			</a>
-			<a class="flex align-middle mr-2" href="mailto:rtng001@e.ntu.edu.sg">
+			<a class={links} href="mailto:rtng001@e.ntu.edu.sg">
 				<Mail />&nbsp;Email
 			</a>
 		</div>
