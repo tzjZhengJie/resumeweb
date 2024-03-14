4ff01a8a20cc355fd3d41aed7ffe4bed4804706a
Tan Zheng Jie <106938371+tzjZhengJie@users.noreply.github.com>
2024-03-15 01:07:21 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,8 @@
 	<svelte:fragment slot="header">
 		<AppBar background="bg-surface-500-400">
 			<div class="flex items-center top-8">
-				<h2 class="h2 font-bold text-white">{$page.url.pathname}</h2>
-				<a class="anchor fixed right-28" href="/uses">Uses</a>
+				<!-- <h2 class="h2 font-bold text-white">{$page.url.pathname}</h2> -->
+				<!-- <a class="anchor fixed right-28" href="/uses">Uses</a> -->
 				<div class="fixed right-8">
 					<LightSwitch />
 				</div>
